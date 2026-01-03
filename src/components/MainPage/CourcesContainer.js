d7c3ffd024276303ae6425c3c449ac687accf56e
developersssranked <danil.kudryashov.2000@inbox.ru>
2026-01-03 09:45:37 +0300

    Fix

--- src/components/MainPage/CourcesContainer.js
+++ src/components/MainPage/CourcesContainer.js
@@ -5,13 +5,23 @@ import '../../css/MainPage/cources_container.css'
 
 import { useState } from 'react';
 import { getUTCPlus7DateFormatted } from '../../heplers/Utils';
+import { useLocation, useNavigate } from 'react-router-dom';
+import growUpImage from '../../img/grow_up_calc.png'
+
 
 function CourcesContainer({rates, setPageActiveOption}) {
     const [activeOption, setActiveOption] = useState('THB');
+    const { pathname, search } = useLocation();
+    const navigate = useNavigate();
 
     return <div className="main-page-cources-container">
         <div className='main-page-cources-container-actual-text'>Актуально<br/>на {getUTCPlus7DateFormatted()}</div>
         <ToggleSwitch activeOption={activeOption} setActiveOption={setActiveOption} setPageActiveOption={setPageActiveOption}/>
+        {pathname === '/' &&
+            <div className='main-page-cources-grow-up-image-container' onClick={() => navigate('/rates')}>
+                <img className='main-page-cources-grow-up-image' src={growUpImage} alt='grow-up-image'/>
+            </div>
+        }
         <CourcesTable rates={rates} activeOption={activeOption} setPageActiveOption={setPageActiveOption}/>
     </div>
 };
