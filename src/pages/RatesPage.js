44cbde3212c802ec6d233977eb03ed3d99ff5a12
developersssranked <danil.kudryashov.2000@inbox.ru>
2026-01-13 19:29:24 +0500

    Fix

--- src/pages/RatesPage.js
+++ src/pages/RatesPage.js
@@ -39,8 +39,8 @@ function RatesPage({user, rates}) {
     }, [searchParams]);
 
     return <div className="rates-page">
-        {(!isInputActive && ['android', 'ios'].includes(window.Telegram.WebApp.platform)) && <Header/>}
-        {(!isInputActive && ['android', 'ios'].includes(window.Telegram.WebApp.platform)) && 
+        {(!isInputActive || !(['android', 'ios'].includes(window.Telegram.WebApp.platform))) && <Header/>}
+        {(!isInputActive || !(['android', 'ios'].includes(window.Telegram.WebApp.platform))) && 
         <ToggleSwitchRates activeOption={activeOption} setActiveOption={setActiveOption}/>}
         {activeOption === 'Курс' ? 
             <CourcesContainer rates={rates} setPageActiveOption={setActiveOption}/> : 
@@ -51,10 +51,10 @@ function RatesPage({user, rates}) {
             activeUpperCurrency={activeUpperCurrency} setActiveUpperCurrency={setActiveUpperCurrency}
             activeDownCurrency={activeDownCurrency} setActiveDownCurrency={setActiveDownCurrency} setInputActive={setInputActive}/>
         }
-        {(!isInputActive && ['android', 'ios'].includes(window.Telegram.WebApp.platform)) && 
+        {(!isInputActive || !(['android', 'ios'].includes(window.Telegram.WebApp.platform))) && 
             <ExchangeSection isFixRate={activeOption === 'calc' ? true : false} dialogId={user?.dialog_id} fiatSum={fiatSum} resultSum={resultSum} finalPercent={finalPercent} finalRate={finalRate} activeUpperCurrency={activeUpperCurrency} activeDownCurrency={activeDownCurrency} rates={rates} user={user}/>
         }
-        {(!isInputActive && ['android', 'ios'].includes(window.Telegram.WebApp.platform)) && 
+        {(!isInputActive && !(['android', 'ios'].includes(window.Telegram.WebApp.platform))) && 
             <Fragment>
                 <Breakline/>
                 <Footer/>
